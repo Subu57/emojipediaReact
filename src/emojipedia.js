a3bc0566f372b376a792295bad2b4754a50413dd
Subu57 <42605018+Subu57@users.noreply.github.com>
2020-07-13 09:28:17 +0530

    Initial commit

---
+++ src/emojipedia.js
@@ -0,0 +1,68 @@
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "💪",
+    name: "Tense Biceps",
+    meaning:
+      "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+  },
+  {
+    id: 2,
+    emoji: "🙏",
+    name: "Person With Folded Hands",
+    meaning:
+      "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
+  },
+  {
+    id: 3,
+    emoji: "🤣",
+    name: "Rolling On The Floor, Laughing",
+    meaning:
+      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+  },
+  {
+    id:4,
+    emoji: "😀",
+    name: "Grinning Face",
+    meaning:
+      "A yellow face with simple, open eyes and a broad, open smile, showing upper teeth and tongue on some platforms. Often conveys general pleasure and good cheer or humor."
+  },  
+  {
+    id:5,
+    emoji: "😍",
+    name: "Smiling Face with Heart-Eyes",
+    meaning:
+      "A yellow face with an open smile, sometimes showing teeth, and red, cartoon-styled hearts for eyes. Often conveys enthusiastic feelings of love, infatuation, and adoration, e.g., I love/am in love with this person or thing."
+  },
+  {
+    id:6,
+    emoji: "🖖",
+    name: "Vulcan Salute or Spock",
+    meaning:
+      "A raised hand, with the fingers separated between the ring finger and the middle finger."
+  }, 
+  {
+    id:7,
+    emoji: "🥴",
+    name: "Woozy Face",
+    meaning:
+      "A yellow face with a crumpled mouth and a cockeyed expression, as if tired and emotional from inebriation or smitten with love. Depicted with raised or furrowed eyebrows and at least one eye half-open. WhatsApp's design features a tongue hanging out. "
+  },
+  {
+    id:8,
+    emoji: "😭",
+    name: "Loudly Crying Face",
+    meaning:
+      "A yellow face with an open mouth wailing and streams of heavy tears flowing from closed eyes. May convey inconsolable grief but also other intense feelings, such as uncontrollable laughter or overwhelming joy."
+  },
+  {
+    id:9,
+    emoji: "💋",
+    name: "Kiss Mark",
+    meaning:
+      "The mark left after a firm kiss is placed with bright lipstick. Used in place of ‘xxx’ (kisses), or to send a kiss to someone. More seductive than the kiss emoji."
+  }
+
+
+];
+export default emojipedia;
